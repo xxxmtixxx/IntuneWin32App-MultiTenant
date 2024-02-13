970c2190fb471dd347960eb43a0784fad7fb2376
xxxmtixxx <clemons.paul.l@gmail.com>
2024-02-12 18:19:12 -0600

    Updated self signed cert script

--- Public/Create-SelfSignedCertificate.ps1
+++ Public/Create-SelfSignedCertificate.ps1
@@ -1,9 +1,14 @@
-﻿# Function to create a self-signed certificate
+﻿# Function to create a self-signed certificate with tenant information
 function Create-SelfSignedCertificate {
     param (
-        [string]$certificateName
+        [string]$certificateName,
+        [string]$tenantName # New parameter for tenant name
     )
-    Write-Host "Creating self-signed certificate for $certificateName"
-    $cert = New-SelfSignedCertificate -Subject "CN=$certificateName" -CertStoreLocation "cert:\CurrentUser\My" -KeyExportPolicy Exportable -KeySpec Signature
+    $subjectCN = "CN=$certificateName"
+    if ($tenantName) {
+        $subjectCN += " for Tenant: $tenantName" # Append tenant information to the CN
+    }
+    Write-Host "Creating self-signed certificate for $subjectCN"
+    $cert = New-SelfSignedCertificate -Subject $subjectCN -CertStoreLocation "cert:\CurrentUser\My" -KeyExportPolicy Exportable -KeySpec Signature
     return $cert
 }
